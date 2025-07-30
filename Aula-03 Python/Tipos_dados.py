5c2feaafe91e14f6fe1d78b64d632b6222c2f6af
weslleysoares <correweslleysoares@gmail.com>
2025-07-31 00:14:28 +0100

    Atulização type_dados

--- Aula-03 Python/Tipos_dados.py
+++ Aula-03 Python/Tipos_dados.py
@@ -1,2 +1,18 @@
 numero_inteiro = 10
 print(type(numero_inteiro))  # Exibe o tipo do número inteiro   
+
+numero_decimal = 10.5
+print(type(numero_decimal))  # Exibe o tipo do número decimal   
+numero_complexo = 3 + 4j
+print(type(numero_complexo))  # Exibe o tipo do número complexo 
+
+texto = "Olá, Mundo!"
+print(type(texto))  # Exibe o tipo do texto 
+lista = [1, 2, 3, 4, 5]
+print(type(lista))  # Exibe o tipo da lista
+tupla = (1, 2, 3)
+print(type(tupla))  # Exibe o tipo da tupla
+dicionario = {"chave1": "valor1", "chave2": "valor2"}
+print(type(dicionario))  # Exibe o tipo do dicionário
+conjunto = {1, 2, 3}
+print(type(conjunto))  # Exibe o tipo do conjunto
