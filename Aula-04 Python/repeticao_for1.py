57fb720f77bab01391f2d4942a10069054fdb7d5
weslleysoares <correweslleysoares@gmail.com>
2025-08-13 21:59:10 +0100

    commit aula 6 for in

---
+++ Aula-04 Python/repeticao_for1.py
@@ -0,0 +1,22 @@
+repeticao = 10
+for i in range(repeticao):
+    print(i, end=" ") # Imprime a repetição na mesma linha
+print()
+# Fim do programa
+
+for i in range(1, repeticao): # Inicia do 1 até repeticao - 1
+    print(i, end=" ") # Imprime os números na mesma linha
+print()
+
+
+for i in range(5, repeticao): # Inicia do 5 até repeticao - 1
+    print(i, end=" ") # Imprime os números na mesma linha
+print()
+
+for i in range(1, repeticao, 2): # numeros ímpares usando o incremento de 2 que vai de dois em dois
+    print(i, end=" ") # Imprime os números na mesma linha
+print()
+
+for i in range(repeticao, 0, -2): # Contagem regressiva de repeticao até 0, decrementando de 2 em 2
+    print(i, end=" ") # Imprime os números na mesma linha
+print()
